c99d0deb7cfca00941acea61937c7b4b1598f5f5
Sushant <58162933+sushantcode@users.noreply.github.com>
2021-05-21 03:03:40 -0500

    reset

--- android_app/README.md
+++
@@ -1,2 +0,0 @@
-# senior-design-codebase
-#Please find all the documentation provided by Mobile, Web, and Back end part of the application
--- server_web_api/run.py
+++
@@ -1,9 +0,0 @@
-# -*- encoding: utf-8 -*-
-"""
-Copyright (c) 2019 Devi
-"""
-
-from app import app, db
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
