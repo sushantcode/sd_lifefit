931e60442de09cc275539e81bccbf5f4c3ebfcb9
Sushant <58162933+sushantcode@users.noreply.github.com>
2021-05-28 15:01:27 -0500

    user saved locally after login

--- android_app/Code/app/src/main/java/com/example/myapplication/LoginStuff/User.java
+++ android_app/Code/app/src/main/java/com/example/myapplication/LoginStuff/User.java
@@ -7,25 +7,19 @@ Check documentation with the heading POSTMAN RESPONSE to view the response.
  */
 public class User {
 //Creating constructor, getter and setter
-    private String address, bio, city, email, fname, gender, lname, password, phone, state, uname,zipcode;
-    private boolean is_admin, is_email_verified, profile_pic;
-    private int user_id;
+    private String address, city, email, fname, gender, lname, phone, state, uname, zipcode, profile_pic, user_id;
 
-    public User(String address, String bio, String city, String email, String fname, String gender, String lname, String password, String phone, String state, String uname, String zipcode, boolean is_admin, boolean is_email_verified, boolean profile_pic, int user_id) {
+    public User(String address, String city, String email, String fname, String gender, String lname, String phone, String state, String uname, String zipcode, String profile_pic, String user_id) {
         this.address = address;
-        this.bio = bio;
         this.city = city;
         this.email = email;
         this.fname = fname;
         this.gender = gender;
         this.lname = lname;
-        this.password = password;
         this.phone = phone;
         this.state = state;
         this.uname = uname;
         this.zipcode = zipcode;
-        this.is_admin = is_admin;
-        this.is_email_verified = is_email_verified;
         this.profile_pic = profile_pic;
         this.user_id = user_id;
     }
@@ -33,10 +27,6 @@ public class User {
 
     public String getAddress() {
         return address;
-    }
-
-    public String getBio() {
-        return bio;
     }
 
     public String getCity() {
@@ -59,10 +49,6 @@ public class User {
         return lname;
     }
 
-    public String getPassword() {
-        return password;
-    }
-
     public String getPhone() {
         return phone;
     }
@@ -78,20 +64,10 @@ public class User {
     public String getZipcode() {
         return zipcode;
     }
-
-    public boolean isIs_admin() {
-        return is_admin;
-    }
-
-    public boolean isIs_email_verified() {
-        return is_email_verified;
-    }
-
-    public boolean isProfile_pic() {
+    public String isProfile_pic() {
         return profile_pic;
     }
-
-    public int getUser_id() {
+    public String getUser_id() {
         return user_id;
     }
 }
