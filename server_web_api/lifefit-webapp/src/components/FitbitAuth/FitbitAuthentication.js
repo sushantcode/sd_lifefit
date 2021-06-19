8549a38e21422b409ab1fcd2f2b1df32357e68db
Sushant Gupta <sushantgupta@Sushants-MBP.attlocal.net>
2021-06-19 10:46:30 -0500

    auth files added

--- server_web_api/lifefit-webapp/src/components/FitbitAuth/FitbitAuthentication.js
+++ server_web_api/lifefit-webapp/src/components/FitbitAuth/FitbitAuthentication.js
@@ -5,6 +5,8 @@ import FitbitCallback from './FitbitCallback'
 const FitbitAuthentication = ({history}) => {
   const url = history.location.pathname;
   var uid = url.split("/")[2];
+  // Make call to DynamoDb to check if the UID exists, 
+  // otherwise return returns
   return (
       <div>
         {uid? <h2>{uid}</h2> : <h2>No User Found</h2>}
