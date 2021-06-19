f19831d974c05cf12633ee17b9cecca2b4a1be4b
Sushant <58162933+sushantcode@users.noreply.github.com>
2021-06-18 22:57:46 -0500

    web app react added

---
+++ server_web_api/lifefit-webapp/src/components/FitbitAuthentication.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FitbitAuthentication = (props) => {
+    return (
+        <div>
+            <h2>{props.link}</h2>
+        </div>
+    )
+}
+
+export default FitbitAuthentication
