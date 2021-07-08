8b988d3e198f9fcdef9332fbdab4d7ee2d072b1d
Sushant Gupta <sushantgupta@Sushants-MacBook-Pro.local>
2021-07-08 14:27:06 -0500

    tokens script completed

--- lambda_scripts/fitbitSummaryData.py
+++ lambda_scripts/fitbitSummaryData.py
@@ -1,19 +1,14 @@
-from datetime import datetime, timedelta, date
 import csv
-from apiCalls import getActivities
-from apiCalls import getSleeps
+from datetime import datetime
+import calendar
 
-# restingHeartRate,(Out of Range),caloriesOut,min,max,minutes,(Fat Burn),caloriesOut,min,max,minutes,(Cardio),caloriesOut,min,max,minutes,(Peak),caloriesOut,min,max,minutes,
-
-
-def activitiesRelatedData(user_id, access_token, curr_date):
+def activitiesRelatedData(activity_response):
     activity_data = {
             "activeScore": 0,
             "caloriesOut": 0,
             "trackerdistance": 0,
             "loggedActivitiesdistance": 0
         }
-    activity_response = getActivities(user_id, access_token, curr_date)
     activity_data["activeScore"] = activity_response["summary"]["activeScore"]
     activity_data["caloriesOut"] = activity_response["summary"]["caloriesOut"]
     for item in activity_response["summary"]["distances"]:
@@ -23,7 +18,7 @@ def activitiesRelatedData(user_id, access_token, curr_date):
             activity_data["loggedActivitiesdistance"] = item["distance"]
     return activity_data
 
-def sleepRelatedData(user_id, access_token, curr_date):
+def sleepRelatedData(sleep_response):
     sleep_data = {
             "duration": 0,
             "efficiency": 0,
@@ -44,7 +39,7 @@ def sleepRelatedData(user_id, access_token, curr_date):
             "timeToSleep": 0,
             "startTime": ""
         }
-    sleep_response = getSleeps(user_id, access_token, curr_date)
+
     if sleep_response["sleep"]:
         for item in sleep_response["sleep"]:
             if item["isMainSleep"]:
@@ -66,5 +61,94 @@ def sleepRelatedData(user_id, access_token, curr_date):
                 sleep_data["minutesAwake"] = item["minutesAwake"]
                 sleep_data["timeToSleep"] = item["minutesToFallAsleep"]
                 sleep_data["startTime"] = item["startTime"].split("T")[1]
-
     return sleep_data
+
+def heartRelatedData(heart_response):
+    heart_data = {
+            "restingHeartRate": -1,
+            "(Out of Range)": -1,
+            "caloriesOut1": 0,
+            "min1": 0,
+            "max1": 0,
+            "minutes1": 0,
+            "(Fat Burn)": -1,
+            "caloriesOut2": 0,
+            "min2": 0,
+            "max2": 0,
+            "minutes2": 0,
+            "(Cardio)": -1,
+            "caloriesOut3": 0,
+            "min3": 0,
+            "max3": 0,
+            "minutes3": 0,
+            "(Peak)": -1,
+            "caloriesOut4": 0,
+            "min4": 0,
+            "max4": 0,
+            "minutes4": 0
+        }
+
+    if heart_response["activities-heart"]:
+        heart_value = heart_response["activities-heart"][0]["value"]
+        if "restingHeartRate" in heart_value:
+            heart_data["restingHeartRate"] = heart_value["restingHeartRate"]
+
+        for item in heart_value["heartRateZones"]:
+            if item["name"] == "Out of Range":
+                heart_data["caloriesOut1"] = item["caloriesOut"]
+                heart_data["min1"] = item["min"]
+                heart_data["max1"] = item["max"]
+                heart_data["minutes1"] = item["minutes"]
+            elif item["name"] == "Fat Burn":
+                heart_data["caloriesOut2"] = item["caloriesOut"]
+                heart_data["min2"] = item["min"]
+                heart_data["max2"] = item["max"]
+                heart_data["minutes2"] = item["minutes"]
+            elif item["name"] == "Cardio":
+                heart_data["caloriesOut3"] = item["caloriesOut"]
+                heart_data["min3"] = item["min"]
+                heart_data["max3"] = item["max"]
+                heart_data["minutes3"] = item["minutes"]
+            elif item["name"] == "Peak":
+                heart_data["caloriesOut4"] = item["caloriesOut"]
+                heart_data["min4"] = item["min"]
+                heart_data["max4"] = item["max"]
+                heart_data["minutes4"] = item["minutes"]
+    return heart_data
+
+def getFitbitSummary(curr_date, id, activities, sleeps, heart):
+    activities_col = activitiesRelatedData(activities)
+    sleep_col = sleepRelatedData(sleeps)
+    heart_col = heartRelatedData(heart)
+
+    curr_datetime = datetime.now()
+    dayName = calendar.day_name[curr_datetime.weekday()][0:3]
+    month = calendar.month_name[curr_datetime.weekday()][0:3]
+    dayNum = curr_datetime.strftime("%m")
+    year = curr_datetime.strftime("%Y")
+    time = curr_datetime.time().strftime("%H:%M:%S")
+    datetime_now = dayName + " " + month + " " + dayNum + " " + time + " " + "CDT " + year
+
+    fileName = "Date_" + curr_date + "_User_id_" + id + "_hourlydata.csv"
+    with open(fileName, "w", newline="") as file:
+        csv_file = csv.writer(file, delimiter=",")
+        csv_file.writerow(["ID", "Date", "activeScore", "caloriesOut", "trackerdistance", "loggedActivitiesdistance", 
+            "duration", "efficiency", "totalMinutesAsleep", "restingHeartRate", "(Out of Range)", "caloriesOut", 
+            "min", "max", "minutes", "(Fat Burn)", "caloriesOut", "min", "max", "minutes", "(Cardio)", "caloriesOut", 
+            "min", "max", "minutes", "(Peak)", "caloriesOut", "min", "max", "minutes", "deepCount", "deepMinutes", 
+            "deepAvg", "lightCount", "lightMinutes", "lightAvg", "remCount", "remMinutes", "remAvg", "wakeCount", 
+            "wakeMinutes", "wakeAvg", "minutesAwake", "timeToSleep", "startTime"])
+        csv_file.writerow([id, datetime_now, activities_col["activeScore"], activities_col["caloriesOut"], 
+                activities_col["trackerdistance"], activities_col["loggedActivitiesdistance"], 
+                sleep_col["duration"], sleep_col["efficiency"], sleep_col["totalMinutesAsleep"], 
+                heart_col["restingHeartRate"], 
+                heart_col["(Out of Range)"], heart_col["caloriesOut1"], heart_col["min1"], heart_col["max1"], heart_col["minutes1"],
+                heart_col["(Fat Burn)"], heart_col["caloriesOut2"], heart_col["min2"], heart_col["max2"], heart_col["minutes2"],
+                heart_col["(Cardio)"], heart_col["caloriesOut3"], heart_col["min3"], heart_col["max3"], heart_col["minutes3"],
+                heart_col["(Peak)"], heart_col["caloriesOut4"], heart_col["min4"], heart_col["max4"], heart_col["minutes4"],
+                sleep_col["deepCount"], sleep_col["deepMinutes"], sleep_col["deepAvg"], 
+                sleep_col["lightCount"], sleep_col["lightMinutes"], sleep_col["lightAvg"], 
+                sleep_col["remCount"], sleep_col["remMinutes"], sleep_col["remAvg"],
+                sleep_col["wakeCount"], sleep_col["wakeMinutes"], sleep_col["wakeAvg"],
+                sleep_col["minutesAwake"], sleep_col["timeToSleep"], sleep_col["startTime"]])
+    print("Intraday data recorded successfully!")
--- lambda_scripts/main.py
+++ lambda_scripts/main.py
@@ -1,14 +1,19 @@
 import boto3
 from boto3.dynamodb.conditions import Key, Attr
 from botocore import endpoint
+from datetime import timedelta, date
+
+############### Token Api Calls ###################
 from refreshTokens import getNewTokens
+
+############### Health data Api calls #############
+from apiCalls import getActivities, getSleeps, getHeart, getCalories, getDistance
+from apiCalls import getElevation, getFloors, getSteps
+
+############## Formatted data #####################
 from hourlyData import getQuarterlyData
 from sleepData import getSleepData
-
-############### Tests Import ##############
-from apiCalls import getActivities
-from apiCalls import getSleeps
-from apiCalls import getHeart
+from fitbitSummaryData import getFitbitSummary
 
 
 # Global Initialization of AWS dynamoDb
@@ -25,6 +30,14 @@ dynamodb = boto3.resource('dynamodb')
 
 table = dynamodb.Table('FitbitTokens-y243fkkjqreqpiwavsqlwjf62a-dev')
 
+####################### Get current date ###########################
+def getDate():
+    print("Getting reporting date...")
+    today_date = date.today()
+    last_day = today_date - timedelta(days=0)
+    curr_date = last_day.strftime("%Y-%m-%d")
+    print("Reporting date = ", curr_date)
+    return curr_date
 
 ####################### Update new tokens in database ###########################
 def updateToken(newTokens, id):
@@ -55,9 +68,23 @@ if __name__ == "__main__":
     #         print("Updating database with new tokens...")
     #         updateToken(newTokens, eachRecord['id'])
     #         print("Database updated successfully!!!")
-    access_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMkMySjIiLCJzdWIiOiI5Q1BTVzUiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJ3aHIgd3BybyB3bnV0IHdzbGUgd3dlaSB3c29jIHdhY3Qgd3NldCB3bG9jIiwiZXhwIjoxNjI1NTQzMjgxLCJpYXQiOjE2MjU1MTQ0ODF9.dQ3-t_xIaYvC0wqt1N_Hv7WFW5xUHRzMNsHlnjZuiak"
+    access_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMkMySjIiLCJzdWIiOiI5Q1BTVzUiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJ3aHIgd3BybyB3bnV0IHdzbGUgd3dlaSB3c29jIHdhY3Qgd3NldCB3bG9jIiwiZXhwIjoxNjI1NjU5OTA0LCJpYXQiOjE2MjU2MzExMDR9.Wz_ypgEnMdBplcYU0_HJnOktyWBO3kPBCK34vPuTlx0"
     user_id = "9CPSW5"
     id = "2cb32af6-acd1-43e1-91fe-db8e3b695ff5"
+
+    curr_date = getDate()
+
+    # Make Api calls to get all health data
+    activities = getActivities(user_id, access_token, curr_date)
+    sleeps = getSleeps(user_id, access_token, curr_date)
+    heart = getHeart(user_id, access_token, curr_date)
+    calories = getCalories(user_id, access_token, curr_date)
+    distance = getDistance(user_id, access_token, curr_date)
+    elevation = getElevation(user_id, access_token, curr_date)
+    floors = getFloors(user_id, access_token, curr_date)
+    steps = getSteps(user_id, access_token, curr_date)
+
     # getSleepData(user_id, access_token, id)
     # getQuarterlyData(user_id, access_token, id)
-    print(getHeart(user_id, access_token, "2021-07-04"))
+    getFitbitSummary(curr_date, id, activities, sleeps, heart)
+    # print(heart)
