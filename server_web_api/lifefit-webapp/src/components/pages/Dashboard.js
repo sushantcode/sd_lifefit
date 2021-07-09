479cd1fd5648306be823960d53a81611337a70c3
Sushant Gupta <sushantgupta@Sushants-MacBook-Pro.local>
2021-07-08 21:34:28 -0500

    web auth updated

--- server_web_api/lifefit-webapp/src/components/pages/Dashboard.js
+++ server_web_api/lifefit-webapp/src/components/pages/Dashboard.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Auth } from 'aws-amplify';
 
 const Dashboard = () => {
+  const [id, setId] = useState("");
+  useEffect(() => { 
+    Auth.currentUserInfo()
+    .then((data) => {
+      setId(data.id);
+    });
+  })
+  
   return (
     <div>
       <p>
