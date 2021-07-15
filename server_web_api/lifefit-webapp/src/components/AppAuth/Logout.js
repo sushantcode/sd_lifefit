2ea9e6f0364b38a90280df271262b7b0e2255719
Sushant Gupta <sushantgupta@Sushants-MacBook-Pro.local>
2021-07-15 15:06:52 -0500

    auth route fixed

--- server_web_api/lifefit-webapp/src/components/AppAuth/Logout.js
+++ server_web_api/lifefit-webapp/src/components/AppAuth/Logout.js
@@ -1,28 +1,71 @@
-import Auth from '@aws-amplify/auth';
-import { Component } from 'react';
+import React, { useState } from 'react';
+import {Auth} from 'aws-amplify';
 import './ResetPassword.css';
+import { useHistory } from 'react-router-dom';
 
-class Logout extends Component {
-  constructor(props) {
-    super(props);
-  }
+const Logout = () => {
+  const history = useHistory();
+  const [isSuccess, setIsSuccess] = useState(false);
 
-  async logoutHanler(e) {
+  async function logoutHanler(e) {
     e.preventDefault();
-    try {
-      await Auth.signOut();
-    } catch (error) {
-        console.log('error signing out: ', error);
-    }
+      try {
+        await Auth.signOut();
+        setIsSuccess(true);
+      } catch (error) {
+          console.log('error signing out: ', error);
+      }
   }
 
-  render() {
-    return (
-      <button onClick={(e) => this.logoutHanler(e)} className="btn">
-        Log-Out
-      </button>
-    );
+  if (isSuccess) {
+    setIsSuccess(false);
+    history.push('/login');
   }
+
+  return (
+    <button onClick={(e) => logoutHanler(e)} className="btn">
+      Log-Out
+    </button>
+  )
 }
 
 export default Logout;
+
+
+
+
+
+
+
+
+// import Auth from 'aws-amplify';
+// import { Component } from 'react';
+// import './ResetPassword.css';
+
+// class Logout extends Component {
+//   constructor(props) {
+//     super(props);
+//   }
+
+//   async logoutHanler(e) {
+//     e.preventDefault();
+//     try {
+//       await Auth.signOut();
+
+//     } catch (error) {
+//         console.log('error signing out: ', error);
+//     }
+//   }
+
+//   render() {
+//     const history = useHistory();
+
+//     return (
+//       <button onClick={(e) => this.logoutHanler(e)} className="btn">
+//         Log-Out
+//       </button>
+//     );
+//   }
+// }
+
+// export default Logout;
