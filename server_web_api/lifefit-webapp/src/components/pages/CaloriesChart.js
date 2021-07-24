d52229c20ab9d2e1deb2543650f69b310c3ae959
Sushant Gupta <sushantgupta@Sushants-MBP.attlocal.net>
2021-07-24 07:11:57 -0500

    graph with selection done

---
+++ server_web_api/lifefit-webapp/src/components/pages/CaloriesChart.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Line } from 'react-chartjs-2';
+
+const CaloriesChart = (props) => {
+  return (
+    <div>
+      <div className="row">
+        <div className="col">
+        <Line 
+            data= {{
+              labels: props.label,
+              datasets: [
+                {
+                  label: 'Amount of Calories Burnt',
+                  data: props.data,
+                  fill: true,
+                  backgroundColor: props.background,
+                  borderColor: props.borderColor,
+                  borderWidth: 1
+                },
+              ],
+            }}
+            options= {{
+                scales: {
+                  yAxes: [{
+                      ticks: {
+                        beginAtZero: true,
+                      },
+                    }]
+                }
+              }
+            } />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default CaloriesChart;
