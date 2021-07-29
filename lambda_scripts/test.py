62600ae7135adb9203eabb7cd06b63a7cec649c9
Sushant Gupta <sushantgupta@Sushants-MacBook-Pro.local>
2021-07-29 15:55:09 -0500

    fitbit summary script sleep error fixed

--- lambda_scripts/test.py
+++ lambda_scripts/test.py
@@ -1,11 +1,15 @@
 from datetime import datetime, date, timedelta
 import calendar
 
-curr_datetime = datetime.now()
-dayName = calendar.day_name[curr_datetime.weekday()][0:3]
-month = calendar.month_name[curr_datetime.weekday()][0:3]
-dayNum = curr_datetime.strftime("%m")
-year = curr_datetime.strftime("%Y")
-time = curr_datetime.time().strftime("%H:%M:%S")
-datetimeNow = dayName + " " + month + " " + dayNum + " " + time + " " + "CDT " + year
-print(datetimeNow)
+def getDate(x):
+    today_date = date.today()
+    last_day = today_date - timedelta(days=x)
+    curr_date = last_day.strftime("%Y-%m-%d")
+    return curr_date
+
+if __name__ == "__main__":
+    arr = []
+    for x in range(22, 80):
+        arr.append(getDate(x))
+
+    print(arr)
