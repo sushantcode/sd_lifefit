55a28b84c0c6866f5c29595282a56a9eaf3a9e6e
Sushant <58162933+sushantcode@users.noreply.github.com>
2021-12-31 16:17:59 -0600

    Update config.js

--- react_native/HomeComponent/config.js
+++ react_native/HomeComponent/config.js
@@ -4,8 +4,8 @@ export const config = {
         tokenEndpoint: 'https://api.fitbit.com/oauth2/token',
         revocationEndpoint: 'https://api.fitbit.com/oauth2/revoke'
       },
-     clientSecret: 'aea53919e7de0f0ded7e30ea9fa2180b',
-    clientId: '22C2J2',
+     clientSecret: 'xxxxxxxxxxxxxxxxxxxxxxxx',
+    clientId: 'xxxxx',
     redirectUrl: 'com.lifefitapp://fitbit',
     scopes: ['heartrate', 'activity', 'activity' , 'profile' , 'sleep'],
   };
